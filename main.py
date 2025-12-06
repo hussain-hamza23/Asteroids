ec00559da1b26e8af16c28320653e5f0890c12f4
hussain-hamza23 <hzhussain23@gmail.com>
2025-12-05 23:17:14 -0500

    Added asteroids and 'game over' condition upon collision

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame
+import sys
 from constants import SCREEN_HEIGHT , SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+
 
 def main():
     pygame.init()
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print("Screen width: 1280 Screen height: 720")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # set up the drawing window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -15,7 +23,11 @@ def main():
     fps = pygame.time.Clock()
     delta_time = 0
     # creates a new player instance. Parameters are the starting x and y position
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable) 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         #log events
@@ -25,15 +37,26 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        # update all updatable sprites
+        updatable.update(delta_time)    
         # draw the GUI    
         screen.fill("black")
         # draw the player. Must be done before flipping the display.
-        player.draw(screen)
-        pygame.display.flip()
+        # Must iterate through drawable group to access the draw method in each sprite
+        for sprite in drawable:  
+            sprite.draw(screen)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+                return
 
+        pygame.display.flip()
         # lmit to 60 FPS
-        fps.tick(60)
         delta_time = fps.tick(60) / 1000.0
+       
 
         
     
